22c1dad639d8c79073b7182a1baed405a1f0f756
fishnlay <skaer7860@gmail.com>
2025-01-08 23:03:36 +0100

    rounded coerners

--- js/right_click.js
+++ js/right_click.js
@@ -1,6 +1,8 @@
-// Disable right-click for the image
-const img = document.getElementById('myImage');
-img.addEventListener('contextmenu', (e) => {
-    e.preventDefault(); // Prevent the default context menu from appearing
-    alert('Right-click is disabled on this image.');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.getElementById('myImage');
+    img.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      alert('Right-click is disabled on this image.');
+    });
+  });
+  
